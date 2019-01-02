94abdd31791317eae85a7325e84eb83150159c09
Kacey <kaceywan@gmail.com>
2019-01-02 16:08:34 -0600

    Add ability to make posts from the administrator interface.

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,26 @@
-from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Post
+
+admin.site.register(Post)
+'''
+from django.contrib import admin
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+# fields display on change list
+list_display = ['title', 'description']
+# fields to filter the change list with
+list_filter = ['published', 'created']
+# fields to search in change list
+search_fields = ['title', 'description', 'content']
+# enable the date drill down on change list
+date_hierarchy = 'created'
+# enable the save buttons on top on change form
+save_on_top = True
+# prepopulate the slug from the title - big timesaver!
+prepopulated_fields = {"slug": ("title",)}
+
+admin.site.register(Post, PostAdmin)
+'''
